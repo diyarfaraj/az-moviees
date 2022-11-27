894f84f8b67d2cd6a4214bc26c2348fe3711086b
Diyar Faraj <diyar.faraj@gmail.com>
2022-11-28 00:53:41 +0100

    favorite screen in progress

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # az-moviees
+
+Improvements suggestions:
+
+- Favorite screen needs clean up
+- Favorite handling can be extracted into custom hooks
+-
--- src/screens/FavoriteScreen.tsx
+++ src/screens/FavoriteScreen.tsx
@@ -1,19 +1,67 @@
-import React, {PureComponent} from 'react';
-import {View, Text} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import React, {PureComponent, useEffect, useState} from 'react';
+import {View, Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import ShowLIst from '../componentes/ShowList';
+import {Routes} from '../navigation/MainContainer';
+import {RootStackParamList} from '../types/rootStackParamList';
+import {Show} from '../types/Show';
 
-const FavoriteScreen = () => {
+interface FavoriteProps {
+  navigation: NativeStackNavigationProp<
+    RootStackParamList,
+    Routes.RESULT_DETAILS
+  >;
+}
+
+const FavoriteScreen = ({navigation}: FavoriteProps) => {
+  const [shows, setShows] = useState<Show[]>([]);
+
+  const removeFavorite = async (show: Show) => {
+    try {
+      await AsyncStorage.removeItem(`favorite-${show.show.id}`);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getFavorite = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const result = await AsyncStorage.multiGet(keys);
+      let shows: Show[] = [];
+      result.map(req => {
+        // @ts-ignore
+        let obj = JSON.parse(req[1]);
+        let show = obj as Show;
+        shows.push(show);
+        console.log(shows);
+      });
+
+      setShows(shows);
+    } catch (e) {
+      console.log('error in getfavorite', e);
+    }
+  };
+
+  useEffect(() => {
+    getFavorite();
+  }, []);
+
   return (
-    <>
-      {/* {errors ? <Text>{errors}</Text> : null}
-    {loading ? (
-      <ActivityIndicator size={'large'} />
-    ) : (
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <Text style={styles.header}>🎬 Favorite Movies</Text>
       <SafeAreaView>
-        <ShowLIst title="Result" res={result} navigation={navigation} />
+        <ShowLIst title="Result" res={shows} navigation={navigation} />
       </SafeAreaView>
-    )} */}
-    </>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+  },
+});
 
 export default FavoriteScreen;
